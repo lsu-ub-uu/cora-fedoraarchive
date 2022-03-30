d649837b0d972426a4b43c1907a6b0b967fb95ba
olovm <olov.mckie@ub.uu.se>
2022-03-30 15:22:04 +0000

    adding services definition

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,5 @@
+import se.uu.ub.cora.fedoraarchive.FedoraRecordArchiveProvider;
+
 /**
  * The fedora module provides interfaces and classes to use a Fedora Commons System in a Cora based
  * system.
@@ -9,4 +11,7 @@ module se.uu.ub.cora.fedoraarchive {
 	requires se.uu.ub.cora.httphandler;
 	requires se.uu.ub.cora.xmlconverter;
 	requires se.uu.ub.cora.logger;
+
+	provides se.uu.ub.cora.storage.archive.RecordArchiveProvider with FedoraRecordArchiveProvider;
+
 }
